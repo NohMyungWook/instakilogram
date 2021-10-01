2bbf3cb10d52d0eae0b79341f80459e38c4af2a7
NohMW <jeongas9810@naver.com>
2021-10-01 14:49:31 +0900

    btn

--- eunchae/main.js
+++ eunchae/main.js
@@ -4,20 +4,24 @@ $(document).ready(function func(){
         if(($("input[name=ps]").val()!="") & ($("input[name=id]").val()!="")){
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 1)");
             $(".blueBtn").css("cursor", "pointer");
+            $(".blueBtn").prop("disabled", false);
         }
         else{
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 0.3)");
             $(".blueBtn").css("cursor", "default");
+            $(".blueBtn").prop("disabled", true);
         }
     });
     $("input[name=ps]").on("propertychange change keyup paste input", function() {
         if(($("input[name=ps]").val()!="") & ($("input[name=id]").val()!="")){
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 1)");
             $(".blueBtn").css("cursor", "pointer");
+            $(".blueBtn").prop("disabled", false);
         }
         else{
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 0.3)");
             $(".blueBtn").css("cursor", "default");
+            $(".blueBtn").prop("disabled", true);
         }
     });
 
@@ -26,10 +30,12 @@ $(document).ready(function func(){
         if($("#findPw_id").val()!=""){
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 1)");
             $(".blueBtn").css("cursor", "pointer");
+            $(".blueBtn").prop("disabled", false);
         }
         else{
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 0.3)");
             $(".blueBtn").css("cursor", "default");
+            $(".blueBtn").prop("disabled", true);
         }
     });
 
@@ -38,30 +44,36 @@ $(document).ready(function func(){
         if(($("#sign_name").val()!="") & ($("#sign_id").val()!="") & ($("#sign_ps").val()!="")){
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 1)");
             $(".blueBtn").css("cursor", "pointer");
+            $(".blueBtn").prop("disabled", false);
         }
         else{
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 0.3)");
             $(".blueBtn").css("cursor", "default");
+            $(".blueBtn").prop("disabled", true);
         }
     });
     $("#sign_id").on("propertychange change keyup paste input", function() {
         if(($("#sign_name").val()!="") & ($("#sign_id").val()!="") & ($("#sign_ps").val()!="")){
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 1)");
             $(".blueBtn").css("cursor", "pointer");
+            $(".blueBtn").prop("disabled", false);
         }
         else{
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 0.3)");
             $(".blueBtn").css("cursor", "default");
+            $(".blueBtn").prop("disabled", true);
         }
     });
     $("#sign_ps").on("propertychange change keyup paste input", function() {
         if(($("#sign_name").val()!="") & ($("#sign_id").val()!="") & ($("#sign_ps").val()!="")){
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 1)");
             $(".blueBtn").css("cursor", "pointer");
+            $(".blueBtn").prop("disabled", false);
         }
         else{
             $(".blueBtn").css("background-color", "rgba(0, 140, 255, 0.3)");
             $(".blueBtn").css("cursor", "default");
+            $(".blueBtn").prop("disabled", true);
         }
     });
 });
